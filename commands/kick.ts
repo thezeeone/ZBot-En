65df907668a97b86e6d7ac3eedb7b2ce26ec3be0
Zahid556 <76518130+Zahid556@users.noreply.github.com>
2022-07-23 10:54:22 +0100

    Update kick.ts
    Permission checks added
    Comments added
    Check a final `member.kickable` check to see if the member is kickable **before** doing **anything**
    Updated format, will now appear like this:
    _You have been kicked from **ZBot** with reason **spam**._
    _Successfully kicked **user#2022** (`1000095206030454915`) from the server with reason **spam**._
    Bot now informs if the user has been directly messaged.

--- commands/kick.ts
+++ commands/kick.ts
@@ -1,4 +1,5 @@
-import { ApplicationCommandOptionType, ChatInputCommandInteraction, Formatters } from "discord.js"
+import { ApplicationCommandOptionType, ChatInputCommandInteraction, Formatters, GuildMember, PermissionsBitField } from "discord.js"
+import { ordinalNumber, pluralisation } from "../util"
 import { Cmd } from "./command-exports"
 
 const kickCommand: Cmd = {
@@ -21,20 +22,109 @@ const kickCommand: Cmd = {
         ]
     },
     async execute(interaction: ChatInputCommandInteraction<"cached">): Promise<any> {
+        // Input
         const member = (<ChatInputCommandInteraction<"cached">>interaction).options.getMember('member')
         const reason = (<ChatInputCommandInteraction<"cached">>interaction).options.getString('reason')
 
+        // Check if the member is in the server
         if (!member) return await interaction.reply({
             content: 'Cannot find that member.',
             ephemeral: true
         })
 
-        member.kick(`Kicked by ${interaction.user.tag} (${interaction.user.id}) ${reason ? `with reason ${reason}` : 'without reason'}`)
+        // Check if the bot's highest role is higher than the member's highest
+        if (member.roles.highest.position >= (<GuildMember>interaction.guild.members.me).roles.highest.position) {
+            const memberRolePos = member.roles.highest.position
+            const botRolePos = (<GuildMember>interaction.guild.members.me).roles.highest.position
+            const numRoles = interaction.guild.roles.cache.size - 1
+            return await interaction.reply({
+                content: `I cannot kick ${
+                    Formatters.bold(member.user.tag)
+                } (${
+                    Formatters.inlineCode(member.id)
+                }) because their highest role (${
+                    Formatters.inlineCode(member.roles.highest.name)
+                }, ${
+                    numRoles - memberRolePos === 0 ? 'highest role' : `${
+                        Formatters.inlineCode(ordinalNumber(numRoles - memberRolePos))
+                    } highest role`
+                }) is higher than or the same as my highest role (${
+                    Formatters.inlineCode((<GuildMember>interaction.guild.members.me).roles.highest.name)
+                }, ${
+                    memberRolePos === botRolePos 
+                    ? 'same role' 
+                    : `${memberRolePos - botRolePos} ${pluralisation(memberRolePos - botRolePos, 'role')} higher`
+                }).`,
+                ephemeral: true
+            })
+        }
+
+        // Required permissions
+        const perms = new PermissionsBitField('KickMembers').toArray()
+
+        if (
+            !perms.every(perm => (<GuildMember>interaction.guild.members.me).permissions.has(perm))
+        ) {
+            return await interaction.reply({
+                content: `Bot is missing permissions.\nThis command requires the bot to have the ${
+                    perms
+                    .map(
+                        s => Formatters.inlineCode((s.match(/[A-Z][a-z]+/g) as RegExpMatchArray).join(' '))
+                    )
+                } ${
+                    pluralisation(
+                        perms.length,
+                        'permission'
+                    )
+                }. The bot is missing ${
+                    Formatters.bold('this permission')
+                }.`
+            })
+        }
+
+        // Check if the member is kickable apart from any other conditions
+        // This will stop the bot from throwing errors when it kicks the member afterwards
+        if (!member.kickable) return await interaction.reply({
+            content: 'This member is unkickable.',
+            ephemeral: true
+        })
+
+        // Directly message the member and reply, if it doesn't work the bot will inform, and kick anyways
+        member.send(`You have been kicked from ${
+            Formatters.bold(interaction.guild.name)
+        } ${
+            reason 
+            ? `with reason ${Formatters.bold(reason)}` 
+            : 'without a reason'
+        }.`)
         .then(async () => {
-            return await interaction.reply({ content: `Successfully kicked ${Formatters.bold(member.user.tag)} (${member.id}) from the server ${reason ? `with reason ${Formatters.bold(reason)}` : 'without a reason'}.`})
+            await interaction.reply({
+                content: `Successfully kicked ${
+                    Formatters.bold(member.user.tag)
+                } (${
+                    Formatters.inlineCode(member.id)
+                }) from the server ${
+                    reason
+                    ? `with reason ${Formatters.bold(reason)}`
+                    : 'without a reason'
+                }.`
+            })
         })
         .catch(async () => {
-            return await interaction.reply({ content: 'Unable to kick that member.', ephemeral: true })
+            await interaction.reply({
+                content: `Successfully kicked ${
+                    Formatters.bold(member.user.tag)
+                } (${
+                    Formatters.inlineCode(member.id)
+                }) from the server ${
+                    reason
+                    ? `with reason ${Formatters.bold(reason)}`
+                    : 'without a reason'
+                }. I could not DM them.`
+            })
+        })
+        .finally(async () => {
+            await member.kick(`Kicked by ${interaction.user.tag} (${interaction.user.id}) ${reason ? `with reason ${reason}` : 'without a reason'}`)
         })
     }
 }
