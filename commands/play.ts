cd4f990792db5b40a7d94a676d3321fa8d6975a7
Zahid556 <76518130+Zahid556@users.noreply.github.com>
2022-07-24 20:48:46 +0100

    Update play.ts (incomplete)
    (incomplete) Added a queue system, regex checks and an interaction reply so that follow-ups can be repeated. (will be edited on VSC because faster)

--- commands/play.ts
+++ commands/play.ts
@@ -19,17 +19,46 @@ const playCommand: Cmd = {
 		// Get the voice channel of the member
 		const memberVC = interaction.member.voice?.channelId
 
+		// Permission Check
+		const perms = new PermissionsBitField('Speak', 'Connect')
+		
+		if (
+			!perms.every(perm => (<GuildMember>interaction.guild.members.me).permissions.has(perm))
+		) return await interaction.reply({
+			content: 'Bot is missing one of `Connect`, `Speak` permissions.',
+			ephemeral: true
+		})
+		
 		// if the member is not in VC, throw back a message informing the user
 		if (!memberVC) return await interaction.reply({
 			content: 'You must be in a voice channel for this command to work!',
 			ephemeral: true
 		})
-		
+
 		// Get the link
 		const link = interaction.options.getString('yt-vid')
 		
-		// Regex
-		// /(?:(?:https?:)?\/\/)?(?:(?:www\.|m\.)?youtube(?:-nocookie)?\.com|youtu.be)\/(?:watch\?v=([\w-]+)|embed\/([\w-]+)|v\/([\w-]+)|([\w-]+))/gm
+		// Test the URL against a regex to see if it's valid.
+		if (!link.match(
+		     /(?:(?:https?:)?\/\/)?(?:(?:www\.|m\.)?youtube(?:-nocookie)?\.com|youtu.be)\/(?:watch\?v=([\w-]+)|embed\/([\w-]+)|v\/([\w-]+)|([\w-]+))/gm
+		)) return await interaction.reply({
+			content: 'Not a valid YouTube video URL format!',
+			ephemeral: true
+		})
+
+		// Add the user's song to the link
+		queue.set(interaction.user.id, link)
+		await interaction.reply({
+			content: `[Song](${link}) added to queue.`,
+			ephemeral: true
+		})
+
+		// Repeat the queue.
+		do {
+			if (queue.size === 0) {
+				
+			}
+		} while (queue.size > -1)
 	}
 }
 
