ee012df2ad918d8ea607e54555f2194c8545106e
Zahid556 <76518130+Zahid556@users.noreply.github.com>
2022-07-25 10:51:08 +0100

    Update stop.ts
    Added a stop system that checks if the member is in the same vc as the bot if in any before proceeding to stop the music, clear the queue and leave.

--- commands/stop.ts
+++ commands/stop.ts
@@ -1,7 +1,39 @@
-import { Cmd } from './command-exports'
+import { getVoiceConnection } from '@discordjs/voice'
+import { ChatInputCommandInteraction, GuildMember } from 'discord.js'
+import { Cmd, queue } from './command-exports'
 
 const stopCommand: Cmd = {
+  data: {
+    name: 'stop',
+    description: 'Stop the current song, clear the queue and leave'
+  },
+  async execute(interaction: ChatInputCommandInteraction<"cached">) {
+    // Check the user is in VC
+    if (!interaction.member.voice?.channel) return await interaction.reply({
+      content: 'You must be in VC for this command to work!',
+      ephemeral: true
+    })
 
+    // Get the connection, if any
+    const connection = getVoiceConnection(interaction.guild.id)
+
+    if (connection) {
+      if ((<GuildMember>interaction.guild.members.me).voice.channel?.id === interaction.member.voice.channel?.id) {
+        await interaction.reply('Leaving voice chat...')
+        queue.clear()
+        connection.destroy()
+        await interaction.editReply('Successfully left voice chat. Queue has been cleared.')
+      } else return await interaction.reply({
+        content: 'You must be in the same voice chat as the bot for this to work.',
+        ephemeral: true
+      })
+    } else {
+      return await interaction.reply({
+        content: 'This bot is not in a voice chat!',
+        ephemeral: true
+      })
+    }
+  }
 }
 
 export {
