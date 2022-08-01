d75f2b8399f2314d38aca0c23b1130852aafdbd7
Zahid556 <76518130+Zahid556@users.noreply.github.com>
2022-08-01 23:01:07 +0100

    Couple updates
    Added the data
    Now will think of checking if user is in VC before attempting to make connection

--- commands/play.ts
+++ commands/play.ts
@@ -1,7 +1,22 @@
+import { ApplicationCommandOptionType, ChatInputCommandInteraction } from "discord.js";
 import { Cmd } from "./command-exports";
 
 const playCommand: Cmd = {
-
+    data: {
+        name: 'play',
+        description: 'Play music in voice chat',
+        options: [
+            {
+                name: 'link',
+                description: 'Enter YouTube link (spotify support coming soon)',
+                type: ApplicationCommandOptionType.String,
+                required: true
+            }
+        ]
+    },
+    async execute(interaction: ChatInputCommandInteraction<"cached">) {
+        
+    }
 }
 
 export {
