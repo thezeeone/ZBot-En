ab048acf3cf837a44bc854f09cd38b4d78fe9938
Zahid556 <76518130+Zahid556@users.noreply.github.com>
2022-08-03 13:03:13 +0100

    Update play.ts
    #16 
    Will now compile and test this in both bots

--- commands/play.ts
+++ commands/play.ts
@@ -1,5 +1,13 @@
-import { ApplicationCommandOptionType, ChatInputCommandInteraction, EmbedBuilder, inlineCode } from "discord.js";
+import { ApplicationCommandOptionType, bold, ChatInputCommandInteraction, EmbedBuilder, inlineCode, PermissionsBitField } from "discord.js";
 import { Cmd } from "./command-exports";
+// @ts-ignore
+// --esModuleInterop
+import ytdl = require('ytdl-core')
+import { join } from "node:path";
+import { createWriteStream } from "fs";
+import { AudioPlayerStatus, createAudioPlayer, createAudioResource, entersState, getVoiceConnection, joinVoiceChannel, VoiceConnectionStatus,  } from "@discordjs/voice";
+import { VoiceChannel, GuildMember } from "discord.js";
+import { commaList, pluralise } from "../util";
 
 const playCommand: Cmd = {
     data: {
@@ -67,6 +75,197 @@ const playCommand: Cmd = {
                     }
                 )
             ]
+        })
+
+        const botMember = <GuildMember>interaction.guild.members.me
+
+        const currentConnection = getVoiceConnection(interaction.guild.id)
+
+        if (currentConnection) return await interaction.reply({
+            embeds: [
+                new EmbedBuilder()
+                .setColor(0xff0000)
+                .setTitle('`/play` - Existing connection')
+                .setDescription(`This bot is already playing audio in ${
+                    (<VoiceChannel>botMember.voice.channel).toString()
+                }`)
+            ]
+        })
+
+        const memberChannel = interaction.member.voice?.channel
+
+        if (!memberChannel) return await interaction.reply({
+            embeds: [
+                new EmbedBuilder()
+                .setColor(0xff0000)
+                .setTitle('`/play` - No Voice Channel')
+                .setDescription('You must be in a voice channel for this command to work!')
+            ]
+        })
+
+        const perms = new PermissionsBitField(['Connect', 'Speak']).toArray()
+
+        if (
+            !perms.every(perm => botMember.permissions.has(perm))
+            ) {
+            const missingPerms = perms.filter(p => !botMember.permissions.has(p))
+            return await interaction.reply({
+                embeds: [
+                    new EmbedBuilder()
+                    .setAuthor({
+                        name: `${interaction.user.tag} (${interaction.user.id})`,
+                        iconURL: interaction.user.displayAvatarURL({ forceStatic: false })
+                    })
+                    .setTitle(`${inlineCode('/ban remove')} - Missing Permissions`)
+                    .setDescription(`Bot is missing permissions.\nThis command requires the bot to have the ${
+                        bold(
+                            `${commaList(
+                                perms
+                                .map(
+                                    s => inlineCode((s.match(/[A-Z][a-z]+/g) as RegExpMatchArray).join(' '))
+                                )
+                            )} ${
+                                pluralise(perms.length, 'permissions')
+                            }`
+                        )
+                    }.\nThe bot has the ${
+                        bold(
+                            `${commaList(
+                                perms
+                                .filter(
+                                    p => !missingPerms.includes(p)
+                                )
+                                .map(
+                                    s => inlineCode((s.match(/[A-Z][a-z]+/g) as RegExpMatchArray).join(' '))
+                                )
+                            )} ${
+                                pluralise(perms.filter(p => !missingPerms.includes(p)).length, 'permissions')
+                            }`
+                        )
+                    }, however is __missing__ the ${
+                        bold(
+                            `${commaList(
+                                missingPerms
+                                .map(
+                                    s => inlineCode((s.match(/[A-Z][a-z]+/g) as RegExpMatchArray).join(' '))
+                                )
+                            )} ${
+                                pluralise(missingPerms.length, 'permissions')
+                            }`
+                        )
+                    }.`)
+                    .setColor(0xff0000)
+                ],
+                ephemeral: true
+            })
+        }
+
+        const createConnection = joinVoiceChannel({
+            guildId: interaction.guild.id,
+            channelId: interaction.guild.id,
+            adapterCreator: interaction.guild.voiceAdapterCreator
+        })
+
+        await interaction.reply({
+            embeds: [
+                new EmbedBuilder()
+                .setTitle('Connecting to VC')
+                .setDescription(`Joining ${memberChannel.toString()}...`)
+                .setColor(0xffff00)
+            ]
+        })
+
+        createConnection.on(VoiceConnectionStatus.Ready, async () => {
+            await interaction.editReply({
+                embeds: [
+                    EmbedBuilder.from((await interaction.fetchReply()).embeds[0])
+                    .setTitle('Ready to Play Audio')
+                    .setDescription(`The bot is ready to play audio in ${memberChannel.toString()}!`)
+                    .setColor(0x00ff00)
+                ]
+            })
+
+            const audioPlayer = createAudioPlayer()
+
+            ytdl(link)
+                .pipe(createWriteStream(`./ytfiles/${videoId}.mp3`))
+
+            const videoInfo = await ytdl.getInfo(link)
+
+            const audioResource = createAudioResource(join(__dirname, `./ytfiles/${videoId}.mp3`), { inlineVolume: true })
+            audioResource.volume?.setVolume(1)
+
+            console.log(videoInfo)
+
+            audioPlayer.on(AudioPlayerStatus.Playing, async () => {
+                await interaction.editReply({
+                    embeds: [
+                        EmbedBuilder.from((await interaction.fetchReply()).embeds[0])
+                        .setTitle('Playing Audio')
+                        .setDescription(`Playing audio in ${memberChannel.toString()}!`)
+                        .setColor(0x00ffff)
+                    ]
+                })
+            })
+
+            audioPlayer.on(AudioPlayerStatus.Idle, async () => {
+                createConnection.destroy()
+                await interaction.channel?.send({
+                    embeds: [
+                        new EmbedBuilder()
+                        .setColor(0xff7700)
+                        .setTitle(`Audio Finished`)
+                        .setDescription(`The audio has finished. Disconnecting from ${memberChannel.toString()}.`)
+                    ]
+                })
+            })
+        })
+
+        createConnection.on(VoiceConnectionStatus.Disconnected, async () => {
+            await interaction.editReply({
+                embeds: [
+                    EmbedBuilder.from((await interaction.fetchReply()).embeds[0])
+                    .setTitle('Re-connection attempt')
+                    .setDescription(`The connection to ${memberChannel.toString()} was cut off, I am attempting to re-connect so please give me 10 seconds...`)
+                    .setColor(0xffff00)
+                ]
+            })
+            Promise.race([
+                entersState(createConnection, VoiceConnectionStatus.Signalling, 10000),
+                entersState(createConnection, VoiceConnectionStatus.Connecting, 10000)
+            ])
+            .then(async () => {
+                await interaction.editReply({
+                    embeds: [
+                        EmbedBuilder.from((await interaction.fetchReply()).embeds[0])
+                        .setTitle('Successful Re-connection')
+                        .setDescription(`I have successfully re-connected to ${memberChannel.toString()}!`)
+                        .setColor(0x00ff00)
+                    ]
+                })
+            })
+            .catch(async () => {
+                createConnection.destroy()
+                await interaction.editReply({
+                    embeds: [
+                        EmbedBuilder.from((await interaction.fetchReply()).embeds[0])
+                        .setTitle('Re-connection Attempt Failed')
+                        .setDescription(`Attempt to re-connect to ${memberChannel.toString()} failed... try again.`)
+                        .setColor(0xff0000)
+                    ]
+                })
+            })
+        })
+
+        createConnection.on(VoiceConnectionStatus.Destroyed, async () => {
+            await interaction.editReply({
+                embeds: [
+                    EmbedBuilder.from((await interaction.fetchReply()).embeds[0])
+                    .setTitle('Connection Destroyed')
+                    .setColor(0xff0000)
+                    .setDescription(`Connection to ${memberChannel.toString()} has been manually destroyed, try again.`)
+                ]
+            })
         })
     }
 }
