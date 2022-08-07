5f63e9aa881ff75eccdcab0ae1f364e960362574
Zahid556 <76518130+Zahid556@users.noreply.github.com>
2022-08-07 09:40:00 +0100

    Remove play.ts, queue, QueueConstruct and SongInfo
    #16

--- commands/command-exports.ts
+++ commands/command-exports.ts
@@ -5,28 +5,6 @@ interface Cmd {
     permissions?: ApplicationCommandPermissionType[],
     execute(interaction: ChatInputCommandInteraction<"cached"> | ContextMenuCommandInteraction<"cached">): any
 }
-
-interface QueueConstruct {
-    textChannel: TextBasedChannelResolvable,
-    voiceChannel: TextBasedChannelResolvable,
-    connection: VoiceConnection | null,
-    songs: SongInfo[],
-    recursive: boolean
-}
-
-interface SongInfo { 
-    url: string,
-    title: string,
-    author: string,
-    authorChannelUrl: string
-    duration: number,
-    thumbnail: string,
-    suggestedBy: User,
-    playing: boolean
-}
-
-const queue = new Map<GuildResolvable, QueueConstruct>()
-
 import { rankCommand } from "./rank";
 import { leaderboardCommand } from "./leaderboard";
 import { timeoutCommand } from "./timeout";
@@ -37,14 +15,10 @@ import { gtwCommand } from "./guesstheword";
 import { memoryGameCommand } from "./memorygame";
 import { blacklistCommand } from "./blacklist";
 import { reportCommand } from "./reportproblem";
-import { playCommand } from "./play";
 import { VoiceConnection } from "@discordjs/voice";
 
 export {
     Cmd,
-    queue,
-    QueueConstruct,
-    SongInfo,
     rankCommand,
     leaderboardCommand,
     timeoutCommand,
@@ -54,6 +28,5 @@ export {
     gtwCommand,
     memoryGameCommand,
     blacklistCommand,
-    playCommand,
     reportCommand
 }
